eda8df06b3a28fc508d68ebcb88f7545db5ac4f6
jayantmanapure1 <jayantmanapure5@gmail.com>
2022-08-09 15:08:44 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,85 @@
+const express= require('express');
+const mongoose= require('mongoose');
+const User = require('./models/User');
+const Message = require('./models/Message');
+
+const app =express();
+app.use(express.json());
+
+
+const PORT=5000;
+
+MONGODB_URI="mongodb+srv://Jayant:jayant%40123@jayant.g7nj8.mongodb.net/chitchat?retryWrites=true&w=majority"
+
+mongoose.connect(MONGODB_URI,()=>{
+   console.log("Connected to MongoDB");
+})
+
+app.get('/health',(req,res)=>{
+   res.json({
+      Status:"ALL GOOD 👍"
+   })
+})
+
+app.post('/signup', async(req, res)=>{
+   const fullName = req.body.fullName;
+   const email = req.body.email;
+   const password = req.body.password;
+ 
+   const newUser = new User({
+     fullName: fullName,
+     email: email,
+     password: password
+   })
+ 
+   const savedUser = await newUser.save();
+ 
+   res.send(savedUser);
+ })
+
+ app.post('/login', async(req, res)=>{
+   const email = req.body.email;
+   const password = req.body.password;
+ 
+   const user = await User.findOne({
+     email: email,
+     password: password
+   })
+ 
+   if(user){
+     res.send({
+       success: true,
+       data: user
+     })
+   }
+   else
+   {
+     res.send({
+       success: false,
+       data: "Wrong Credentials! Please try again."
+     })
+   }
+ })
+
+ app.post('/send', async(req, res)=>{
+   const to = req.body.to;
+   const from = req.body.from;
+   const text = req.body.text;
+ 
+   const newMessage = new Message({
+     to: to,
+     from: from,
+     text: text
+   })
+ 
+   const savedMessage = await newMessage.save();
+ 
+   res.send(savedMessage)
+ })
+ app.get('/messages', async(req, res)=>{
+   const messages = await Message.find();
+   res.send(messages);
+ }) 
+app.listen(PORT, ()=>{
+   console.log('Server started running on port',PORT);
+})
